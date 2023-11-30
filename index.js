a4185a6967b2c971b38f7f6b259a39ca36b5c520
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-11-30 20:48:07 +0600

    get dummy address data from DB

--- index.js
+++ index.js
@@ -30,10 +30,71 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const serviceCollection = client.db("cypherTechDB").collection("services")
+    const serviceCollection = client.db("cypherTechDB").collection("services");
+    const reviewCollection = client.db("cypherTechDB").collection("testimonials");
+    const userCollection = client.db("cypherTechDB").collection("users");
+    const dummyCollection = client.db("cypherTechDB").collection("dummy");
 
 
+    // dummy location
 
+    app.get('/dummy', async(req, res)=> {
+        const result = await dummyCollection.find().toArray()
+        res.send(result)
+    })
+
+
+    // jwt related
+    app.post('/jwt', async(req, res)=> {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1h"})
+        res.send({token})
+      })
+
+    // middlewares 
+    const verifyToken = (req, res, next) => {
+        console.log('inside verify token',req.headers.authorization);
+        if (!req.headers.authorization) {
+          return res.status(401).send({message: 'Forbidden Access'})
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=> {
+          if (err) {
+            return res.status(401).send({message: 'Forbidden Access'})
+          }
+          req.decoded = decoded;
+          next();
+        })
+        
+      }
+  
+
+    // user related 
+
+    // make users secure
+    //  verifyToken,
+    app.get('/users', async(req, res)=>{
+        const result = await userCollection.find().toArray()
+        res.send(result)
+    })
+    app.post('/users', async(req, res)=> {
+        const user = req.body;
+  
+        const query = {email: user.email}
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res.send({message: 'user exist', insertedId: null})
+        }
+        const result = await userCollection.insertOne(user)
+        res.send(result);
+      })
+
+
+    // testimonials
+    app.get('/testimonials', async(req, res)=> {
+        const result = await reviewCollection.find().toArray()
+        res.send(result);
+    })
     // service related
     app.get('/services', async(req, res)=> {
         const result = await serviceCollection.find().toArray()
